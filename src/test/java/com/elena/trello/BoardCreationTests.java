a98e11a47a62153833cb2e189807b2e40f50dba4
Lena <etarnovskaya@gmail.com>
2021-04-12 23:19:07 +0300

    replaced methods

--- src/test/java/com/elena/trello/BoardCreationTests.java
+++ src/test/java/com/elena/trello/BoardCreationTests.java
@@ -1,5 +1,6 @@
 package com.elena.trello;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
@@ -8,13 +9,32 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class BoardCreationTests extends TestBase{
-
-    @Test
-    public void testBoardCreation() throws InterruptedException {
+    @BeforeMethod
+    public void preconditions() throws InterruptedException {
         clickLoginButton();
         fillLoginForm();
         confirmLogin();
+    }
+
+
+    @Test
+    public void testAtlassianLogin() {
+        initBoardCreationFoardsList();
+        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).click();
+        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).clear();
+        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).sendKeys("board");
+
+        wd.findElement(By.xpath("//*[@aria-label='DownIcon']/../..")).click();
+        wd.findElement(By.xpath("//*[@aria-label='PublicIcon']/../..")).click();
+
+        wd.findElement(By.cssSelector("._21upOlzpUQJcdT.gkv95EhjCrfcEU")).click();
+        wd.findElement(By.cssSelector("[data-test-id='create-board-submit-button']")).click();
 
 
     }
+
+    public void initBoardCreationFoardsList() {
+        wd.findElement(By.xpath("//*[@data-test-id='create-board-tile']")).click();
+    }
+
 }
--- src/test/java/com/elena/trello/LoginAtlassianAccountTest.java
+++ src/test/java/com/elena/trello/LoginAtlassianAccountTest.java
@@ -10,32 +10,13 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class LoginAtlassianAccountTest extends TestBase{
-
-    @BeforeMethod
-    public void preconditions() throws InterruptedException {
+    @Test
+    public void testBoardCreation() throws InterruptedException {
         clickLoginButton();
         fillLoginForm();
         confirmLogin();
-    }
-
-
-    @Test
-    public void testAtlassianLogin() {
-        initBoardCreationFoardsList();
-        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).click();
-        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).clear();
-        wd.findElement(By.xpath("//input[@data-test-id='create-board-title-input']")).sendKeys("board");
-
-        wd.findElement(By.xpath("//*[@aria-label='DownIcon']/../..")).click();
-        wd.findElement(By.xpath("//*[@aria-label='PublicIcon']/../..")).click();
-
-        wd.findElement(By.cssSelector("._21upOlzpUQJcdT.gkv95EhjCrfcEU")).click();
-        wd.findElement(By.cssSelector("[data-test-id='create-board-submit-button']")).click();
 
 
     }
 
-    public void initBoardCreationFoardsList() {
-        wd.findElement(By.xpath("//*[@data-test-id='create-board-tile']")).click();
-    }
 }
