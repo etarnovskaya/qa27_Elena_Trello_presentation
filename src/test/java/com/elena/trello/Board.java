f1082d206e3f78edd15e0cd39bc93adcf41f7e03
Lena <etarnovskaya@gmail.com>
2021-04-22 21:42:13 +0300

    fluent interface

---
+++ src/test/java/com/elena/trello/Board.java
@@ -0,0 +1,19 @@
+package com.elena.trello;
+
+public class Board {
+    private  String boardName;
+    private  String visibility;
+
+    public Board(String boardName, String visibility) {
+        this.boardName = boardName;
+        this.visibility = visibility;
+    }
+
+    public String getBoardName() {
+        return boardName;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+}
